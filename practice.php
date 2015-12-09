142fc1c301206f1674be7827f576e9f9e38efab4
gjorgjim <gjorgjimarkov@yahoo.com>
2015-12-10 00:23:15 +0100

    Added practice page.

---
+++ practice.php
@@ -0,0 +1,71 @@
+<html>
+	<head>
+		<title>Home</title>
+		<meta charset="utf-8" />
+		<link href="styles/clear.css" rel="stylesheet" type="text/css" />
+		<link href="styles/main.css" rel="stylesheet" type="text/css" />
+		<link href="styles/practice.css" rel="stylesheet" type="text/css" />
+		<link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,300italic,700&subset=latin,cyrillic' rel='stylesheet' type='text/css' />
+	</head>
+	<body>
+	<header>
+			<div id="userbar">
+				<h6 id="welcome">Добредојдовте, <a href="#">username.</a></h6>
+				<h6 id="logout"><a href="#">Одјави се</a></h6>
+			</div>
+			<br style="clear:both;"/>
+			<img src="images/title_logo.png" />
+			<nav>
+				<a href="index.php" id="home">ДОМА</a>
+				<a href="learn.php" id="tuts">НАУЧИ ДА КОДИРАШ</a>
+				<a href="practice.php" id="practice">ВЕЖБИ И ЗАДАЧИ</a>
+				<a href="questions.php" id="questions">ИМАШ ПРАШАЊЕ?</a>
+			</nav>
+		</header>
+		<div id="main">
+		<h2>C++/Java задачи:</h2>
+			<div id="zadacha1" class="zadacha">
+				<div id="topdiv">
+				<h1>Топчиња:</h1>
+				</div>
+				<div id="botdiv">
+				<p>асдкајдлксндлкасднкандскјасднлансдклајсндкјансдкј</p>
+				</div>
+			</div>
+			<div  id="zadacha2" class="zadacha">
+				<div id="topdiv">
+				<h1>Топчиња:</h1>
+				</div>
+				<div id="botdiv">
+				<p>асдкајдлксндлкасднкандскјасднлансдклајсндкјансдкј</p>
+				</div>
+			</div>
+			<br />
+			<h2>HTML/JavaScript задачи:</h2>
+			<div id="zadacha1" class="zadacha">
+				<div id="topdiv">
+				<h1>Топчиња:</h1>
+				</div>
+				<div id="botdiv">
+				<p>асдкајдлксндлкасднкандскјасднлансдклајсндкјансдкј</p>
+				</div>
+			</div>
+			<div  id="zadacha2" class="zadacha">
+				<div id="topdiv">
+				<h1>Топчиња:</h1>
+				</div>
+				<div id="botdiv">
+				<p>асдкајдлксндлкасднкандскјасднлансдклајсндкјансдкј</p>
+				</div>
+			</div>
+			<br />
+		</div>
+		<img src="images/register-undershade.png" style="margin: auto; width: 960px;" />
+		<footer>
+			<p id="copyright">COPYRIGHT &copy; BINARY.MK, 2015. РАЗВИЕНО ОД АНДРЕЈ НАУМОВСКИ И ЃОРЃИ МАРКОВ. СИТЕ ПРАВА СЕ ЗАДРЖАНИ</p>
+			<p id="contact"><a href="#">КОНТАКТ</a></p>
+			<img src="images/register-undershade.png" />
+		</footer>
+	</body>
+
+</html>
