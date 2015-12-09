4b4d48dc48d5f28adb7e278da787a9a1dcb399cb
andrej-naumovski <a.naumovski996@yahoo.com>
2015-12-10 00:05:42 +0100

    Refactored PHP code

--- scripts/hash.php
+++ scripts/hash.php
@@ -5,20 +5,20 @@
 	define("HASH_INDEX", 0);
 	define("SALT_INDEX", 1);
 
-	function createHash($password) {
+	function create_hash($password) {
 		$salt = bin2hex(openssl_random_pseudo_bytes(SALT_BYTE_SIZE));
 		return hash(SHA256_HASH_ALGORITHM, $password . $salt) . ":" . $salt;
 	}
 
-	function checkPassword($password, $correctHash) {
+	function check_password($password, $correctHash) {
 		$params = explode(":", $correctHash);
 		if(count($params) < HASH_SECTIONS) {
 			return false;
 		}
-		return slowEquals($params[HASH_INDEX], hash(SHA256_HASH_ALGORITHM, $password . $params[SALT_INDEX]));
+		return slow_equals($params[HASH_INDEX], hash(SHA256_HASH_ALGORITHM, $password . $params[SALT_INDEX]));
 	}
 
-	function slowEquals($a, $b) {
+	function slow_equals($a, $b) {
 		$diff = strlen($a) ^ strlen($b);
 		for($i = 0;$i < strlen($a) && $i < strlen($b); $i++) {
 			$diff |= ord($a[$i]) ^ ord($b[$i]);
